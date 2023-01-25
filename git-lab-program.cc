426378ba3d08f2495ed1eedab602030dc4b722dc
Matt Taylor <mt939121@ohio.edu>
2023-01-24 20:03:18 -0500

    Update

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Matt Taylor
+ *        Date: 1/24/23
+ * Description: Lab 2 file
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
